0d9b4b6573af492dc90ee51716dc994f97e0d44b
Diana Gakuya <deegakuya@gmail.com>
2023-09-19 12:40:38 +0300

    Assignment 4

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+// Challenge 1: Multiply num1 and num2 to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Challenge 2: Generate a random integer greater than 0
+const random = Math.ceil(Math.random() * 100); // Generates a random integer between 1 and 100
+
+// Challenge 3: Calculate the remainder of dividing num3 by num4 to be 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Challenge 4: Find the highest number in a set (values)
+const values = [10, 15, 20, 5, 3];
+const max = Math.max(...values);
+
+console.log("Challenge 1: multiply =", multiply);
+console.log("Challenge 2: random =", random);
+console.log("Challenge 3: mod =", mod);
+console.log("Challenge 4: max =", max);
+
